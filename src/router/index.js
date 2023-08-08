852fe4c3e46a49486d30abaae3f5c41b7362a8ef
wq1997 <2957009914@qq.com>
2023-08-08 16:14:01 +0800

    feat

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 const App = "@/pages/App";
 const Layout = "@/layouts/baseLayout";
 
+const Home = "@/pages/home";
+
+
 const Login = "@/pages/account/login";
 
 export default Routes = [
@@ -12,7 +15,8 @@ export default Routes = [
         path: '/cet',
         component: Layout,
         routes: [
-          
+          // 首页
+          { path: '/cet/home', component: Home },
         ]
       },
       { path: "/login", component: Login }
