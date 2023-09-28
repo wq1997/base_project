5a2af5ac820451b9a9c218921d7f4e69893c5429
wq1997 <2957009914@qq.com>
2023-09-28 11:01:31 +0800

    fix

--- src/pages/resourceManage/topology.jsx
+++ src/pages/resourceManage/topology.jsx
@@ -28,7 +28,7 @@ const Topology = ({data}) => {
                   y: -2,
                 },
                 draggable: true,
-                name: 'label-shape'
+                name: 'label-shape1'
               });
               group.addShape('text', {
                 attrs: {
@@ -39,7 +39,7 @@ const Topology = ({data}) => {
                   y: 4,
                 },
                 draggable: true,
-                name: 'sublabel-shape'
+                name: 'sublabel-shape2'
               });
               group.addShape('text', {
                 attrs: {
@@ -50,7 +50,7 @@ const Topology = ({data}) => {
                   y: 4,
                 },
                 draggable: true,
-                name: 'sublabel-shape'
+                name: 'sublabel-shape3'
               });
               group.addShape('text', {
                 attrs: {
@@ -61,7 +61,7 @@ const Topology = ({data}) => {
                   y: 10,
                 },
                 draggable: true,
-                name: 'sublabel-shape'
+                name: 'sublabel-shape4'
               });
               group.addShape('text', {
                 attrs: {
@@ -72,251 +72,287 @@ const Topology = ({data}) => {
                   y: 10,
                 },
                 draggable: true,
-                name: 'sublabel-shape'
+                name: 'sublabel-shape5'
               });
               return keyShape;
             }
           });
           
-          G6.registerNode('level1node', {
-            draw: (cfg, group) => {
-              const size = [60, 40]
-              const keyShape = group.addShape('rect', {
-                attrs: {
-                  width: size[0],
-                  height: size[1],
-                  x: -size[0] / 2,
-                  y: -size[1] / 2,
-                  fill: 'rgb(213, 225, 247)',
-                  radius: 5
-                },
-                draggable: true,
-                name: 'level1node-keyshape'
-              });
-              group.addShape('text', {
-                attrs: {
-                  text: `${cfg.label}`,
-                  fill: 'rgba(19, 33, 92, 0.85)',
-                  fontSize: 6,
-                  x: 0,
-                  y: -6,
-                  textAlign: 'center'
-                },
-                draggable: true,
-                name: 'label-shape'
-              });
-              group.addShape('text', {
-                attrs: {
-                  text: `${cfg.subLabel1}`,
-                  fill: 'rgba(19, 33, 92, 0.75)',
-                  fontSize: 3,
-                  x: -size[0] / 2 + 5,
-                  y: 4,
-                },
-                draggable: true,
-                name: 'sublabel-shape'
-              });
-              group.addShape('text', {
-                attrs: {
-                  text: `${cfg.subValue1}`,
-                  fill: '#ff6a00',
-                  fontSize: 3,
-                  x: -size[0] / 2 + 40,
-                  y: 4,
-                },
-                draggable: true,
-                name: 'sublabel-shape'
-              });
-              group.addShape('text', {
-                attrs: {
-                  text: `${cfg.subLabel2}`,
-                  fill: 'rgba(19, 33, 92, 0.75)',
-                  fontSize: 3,
-                  x: -size[0] / 2 + 5,
-                  y: 10,
-                },
-                draggable: true,
-                name: 'sublabel-shape'
-              });
-              group.addShape('text', {
-                attrs: {
-                  text: `${cfg.subValue2}`,
-                  fill: '#ff6a00',
-                  fontSize: 3,
-                  x: -size[0] / 2 + 40,
-                  y: 10,
-                },
-                draggable: true,
-                name: 'sublabel-shape'
-              });
-              return keyShape;
-            },
-            update: undefined,
-          }, 'rect')
-          
-          G6.registerNode('othernode', {
-            draw: (cfg, group) => {
-              const size = [100, 30];
-              const keyShape = group.addShape('rect', {
-                attrs: {
-                  width: size[0],
-                  height: size[1],
-                  x: -size[0] / 2,
-                  y: -size[1] / 2,
-                  fill: 'rgb(213, 225, 247)',
-                  radius: 5
-                },
-                draggable: true,
-                name: 'level1node-keyshape'
-              });
-              group.addShape('text', {
-                attrs: {
-                  text: `${cfg.label}`,
-                  fill: 'rgba(19, 33, 92, 0.65)',
-                  fontSize: 6,
-                  x: 10 - size[0] / 2,
-                  y: -2,
-                  textAlign: 'left'
-                },
-                draggable: true,
-                name: 'label-shape'
-              });
-              group.addShape('text', {
-                attrs: {
-                  text: `${cfg.subLabel1}`,
-                  fill: 'rgba(19, 33, 92, 0.75)',
-                  fontSize: 3,
-                  x: -size[0] / 2 + 10,
-                  y: 4,
-                },
-                draggable: true,
-                name: 'sublabel-shape'
-              });
-              group.addShape('text', {
-                attrs: {
-                  text: `${cfg.subValue1}`,
-                  fill: '#ff6a00',
-                  fontSize: 3,
-                  x: -size[0] / 2 + 40,
-                  y: 4,
-                },
-                draggable: true,
-                name: 'sublabel-shape'
-              });
-              group.addShape('text', {
-                attrs: {
-                  text: `${cfg.subLabel2}`,
-                  fill: 'rgba(19, 33, 92, 0.75)',
-                  fontSize: 3,
-                  x: -size[0] / 2 + 10,
-                  y: 10,
-                },
-                draggable: true,
-                name: 'sublabel-shape'
-              });
-              group.addShape('text', {
-                attrs: {
-                  text: `${cfg.subValue2}`,
-                  fill: '#ff6a00',
-                  fontSize: 3,
-                  x: -size[0] / 2 + 40,
-                  y: 10,
-                },
-                draggable: true,
-                name: 'sublabel-shape'
-              });
-              return keyShape;
-            },
-            update: undefined
-          }, 'rect')
-          
-          G6.registerEdge('round-poly', {
-            getControlPoints: (cfg) => {
-              const { startPoint, endPoint } = cfg;
-              return [
-                startPoint,
-                {
-                  x: startPoint.x,
-                  y: endPoint.y
-                },
-                endPoint
-              ];
-            }
-        }, 'polyline')
-        G6.Util.traverseTree(data, subtree => {
-            if (subtree.level === undefined) subtree.level = 0;
-            subtree.children?.forEach(child => child.level = subtree.level + 1);
-            switch (subtree.level) {
-              case 0:
-                subtree.type = 'root';
-                break;
-              case 1:
-                subtree.type = 'level1node';
-                break;
-              default:
-                subtree.type = 'othernode';
-            }
-        });
-        const width = ref.current.scrollWidth;
-        const height = ref.current.scrollHeight;
-        const graph = new G6.TreeGraph({
-          container: 'container',
-          width,
-          height,
-          fitView: true,
-          layout: {
-            type: 'compactBox',
-            direction: 'LR',
-            getHGap: function getVGap() {
-              return 5;
-            },
-          },
-          defaultEdge: {
-            type: 'round-poly',
-            sourceAnchor: 0,
-            targetAnchor: 1,
-            style: {
-              radius: 8,
-              stroke: 'rgb(19, 33, 92)'
-            }
-          },
-          defaultNode: {
-            anchorPoints: [
-              [0.9, 0.5],
-              [0, 0.5]
-            ]
-          },
-          nodeStateStyles: {
-            hover: {
-              fill: '#fff',
-              shadowBlur: 30,
-              shadowColor: '#ddd',
-            },
-            operatorhover: {
-              'operator-box': {
-                opacity: 1
+        G6.registerNode('level1node', {
+          draw: (cfg, group) => {
+            const size = [60, 40]
+            const keyShape = group.addShape('rect', {
+              attrs: {
+                width: size[0],
+                height: size[1],
+                x: -size[0] / 2,
+                y: -size[1] / 2,
+                fill: 'rgb(213, 225, 247)',
+                radius: 5
+              },
+              draggable: true,
+              name: 'level1node-keyshape'
+            });
+            group.addShape('text', {
+              attrs: {
+                text: `${cfg.label}`,
+                fill: 'rgba(19, 33, 92, 0.85)',
+                fontSize: 6,
+                x: 0,
+                y: -6,
+                textAlign: 'center'
+              },
+              draggable: true,
+              name: 'label-shape1'
+            });
+            group.addShape('text', {
+              attrs: {
+                text: `${cfg.subLabel1}`,
+                fill: 'rgba(19, 33, 92, 0.75)',
+                fontSize: 3,
+                x: -size[0] / 2 + 10,
+                y: 2,
+              },
+              draggable: true,
+              name: 'sublabel-shape2'
+            });
+            group.addShape('text', {
+              attrs: {
+                text: `${cfg.subValue1}`,
+                fill: '#ff6a00',
+                fontSize: 3,
+                x: -size[0] / 2 + 40,
+                y: 2,
+              },
+              draggable: true,
+              name: 'sublabel-shape3'
+            });
+            group.addShape('text', {
+              attrs: {
+                text: `${cfg.subLabel2}`,
+                fill: 'rgba(19, 33, 92, 0.75)',
+                fontSize: 3,
+                x: -size[0] / 2 + 10,
+                y: 10,
+              },
+              draggable: true,
+              name: 'sublabel-shape4'
+            });
+            group.addShape('text', {
+              attrs: {
+                text: `${cfg.subValue2}`,
+                fill: '#ff6a00',
+                fontSize: 3,
+                x: -size[0] / 2 + 40,
+                y: 10,
+              },
+              draggable: true,
+              name: 'sublabel-shape5'
+            });
+            group.addShape('line', {
+              attrs: {
+                x1: -size[0] / 2,
+                x2: -size[0] / 2 + 6,
+                y1: 0,
+                y2: 0,
+                lineWidth: 1,
+                stroke: 'rgb(19, 33, 92)',
               }
-            }
+            });
+            group.addShape('circle', {
+              attrs: {
+                r: 2,
+                x: -size[0] / 2 + 6,
+                y: 0,
+                fill: 'rgb(19, 33, 92)',
+              }
+            })
+            return keyShape;
           },
-          modes: {
-            default: ['zoom-canvas', 'drag-canvas', 'collapse-expand']
-          }
-        });
+          update: undefined,
+        }, 'rect')  
         
-        graph.on('node:mouseenter', e => {
-          if (e.target.get('name')?.includes('operator')) {
-            graph.setItemState(e.item, 'operatorhover', true);
-          } else {
-            graph.setItemState(e.item, 'hover', true);
-          }
-        })
-        graph.on('node:mouseleave', e => {
-          graph.setItemState(e.item, 'operatorhover', false);
-          graph.setItemState(e.item, 'hover', false);
-        });
+        G6.registerNode('othernode', {
+          draw: (cfg, group) => {
+            const size = [100, 30];
+            const keyShape = group.addShape('rect', {
+              attrs: {
+                width: size[0],
+                height: size[1],
+                x: -size[0] / 2,
+                y: -size[1] / 2,
+                fill: 'rgb(213, 225, 247)',
+                radius: 5
+              },
+              draggable: true,
+              name: 'level2node-keyshape'
+            });
+            group.addShape('text', {
+              attrs: {
+                text: `${cfg.label}`,
+                fill: 'rgba(19, 33, 92, 0.65)',
+                fontSize: 6,
+                x: 10 - size[0] / 2,
+                y: -2,
+                textAlign: 'left'
+              },
+              draggable: true,
+              name: 'label-shape1'
+            });
+            group.addShape('text', {
+              attrs: {
+                text: `${cfg.subLabel1}`,
+                fill: 'rgba(19, 33, 92, 0.75)',
+                fontSize: 3,
+                x: -size[0] / 2 + 10,
+                y: 4,
+              },
+              draggable: true,
+              name: 'sublabel-shape2'
+            });
+            group.addShape('text', {
+              attrs: {
+                text: `${cfg.subValue1}`,
+                fill: '#ff6a00',
+                fontSize: 3,
+                x: -size[0] / 2 + 40,
+                y: 4,
+              },
+              draggable: true,
+              name: 'sublabel-shape3'
+            });
+            group.addShape('text', {
+              attrs: {
+                text: `${cfg.subLabel2}`,
+                fill: 'rgba(19, 33, 92, 0.75)',
+                fontSize: 3,
+                x: -size[0] / 2 + 10,
+                y: 10,
+              },
+              draggable: true,
+              name: 'sublabel-shape4'
+            });
+            group.addShape('text', {
+              attrs: {
+                text: `${cfg.subValue2}`,
+                fill: '#ff6a00',
+                fontSize: 3,
+                x: -size[0] / 2 + 40,
+                y: 10,
+              },
+              draggable: true,
+              name: 'sublabel-shape5'
+            });
+            group.addShape('line', {
+              attrs: {
+                x1: -size[0] / 2,
+                x2: -size[0] / 2 + 6,
+                y1: 0,
+                y2: 0,
+                lineWidth: 1,
+                stroke: 'rgb(19, 33, 92)',
+              }
+            });
+            group.addShape('circle', {
+              attrs: {
+                r: 2,
+                x: -size[0] / 2 + 6,
+                y: 0,
+                fill: 'rgb(19, 33, 92)',
+              }
+            })
+            return keyShape;
+          },
+          update: undefined
+        }, 'rect')
         
-        graph.data(data);
-        graph.render();
+        G6.registerEdge('round-poly', {
+          getControlPoints: (cfg) => {
+            const { startPoint, endPoint } = cfg;
+            return [
+              startPoint,
+              {
+                x: startPoint.x,
+                y: endPoint.y
+              },
+              endPoint
+            ];
+          }
+      }, 'polyline')
+      G6.Util.traverseTree(data, subtree => {
+          if (subtree.level === undefined) subtree.level = 0;
+          subtree.children?.forEach(child => child.level = subtree.level + 1);
+          switch (subtree.level) {
+            case 0:
+              subtree.type = 'root';
+              break;
+            case 1:
+              subtree.type = 'level1node';
+              break;
+            default:
+              subtree.type = 'othernode';
+          }
+      });
+      const width = ref.current.scrollWidth;
+      const height = ref.current.scrollHeight;
+      const graph = new G6.TreeGraph({
+        container: 'container',
+        width,
+        height,
+        fitView: true,
+        layout: {
+          type: 'compactBox',
+          direction: 'LR',
+          getHGap: function getVGap() {
+            return 5;
+          },
+        },
+        defaultEdge: {
+          type: 'round-poly',
+          sourceAnchor: 0,
+          targetAnchor: 1,
+          style: {
+            radius: 8,
+            stroke: 'rgb(19, 33, 92)'
+          }
+        },
+        defaultNode: {
+          anchorPoints: [
+            [0.9, 0.5],
+            [0, 0.5]
+          ]
+        },
+        nodeStateStyles: {
+          hover: {
+            fill: '#fff',
+            shadowBlur: 30,
+            shadowColor: '#ddd',
+          },
+          operatorhover: {
+            'operator-box': {
+              opacity: 1
+            }
+          }
+        },
+        modes: {
+          default: ['zoom-canvas', 'drag-canvas', 'collapse-expand']
+        }
+      });
+      
+      graph.on('node:mouseenter', e => {
+        if (e.target.get('name')?.includes('operator')) {
+          graph.setItemState(e.item, 'operatorhover', true);
+        } else {
+          graph.setItemState(e.item, 'hover', true);
+        }
+      })
+      graph.on('node:mouseleave', e => {
+        graph.setItemState(e.item, 'operatorhover', false);
+        graph.setItemState(e.item, 'hover', false);
+      });
+      
+      graph.data(data);
+      graph.render();
     }
 
     useEffect(()=>{
