39a8e9fcff43adfe595bdc145ccff02ede8e0598
wq1997 <2957009914@qq.com>
2023-10-09 11:06:05 +0800

    fix

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,7 +1,6 @@
 import { ConfigProvider,theme as antdTheme } from "antd";
 import { Outlet, useSelector } from "umi";
 import { ThemeEnum } from "@/components";
-import dayjs from 'dayjs';
 import en_US from 'antd/locale/en_US';
 import zh_CN from 'antd/locale/zh_CN';
 import dayjs from "dayjs";
