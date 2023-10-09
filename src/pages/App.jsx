47163b7bc5f7747f303e701623f3cc3e55c7d845
wq1997 <2957009914@qq.com>
2023-10-09 09:08:06 +0800

    feat: 添加dayjs

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,6 +3,13 @@ import { Outlet, useSelector } from "umi";
 import { ThemeEnum } from "@/components";
 import en_US from 'antd/locale/en_US';
 import zh_CN from 'antd/locale/zh_CN';
+import dayjs from "dayjs";
+import weekday from "dayjs/plugin/weekday";
+import localeData from "dayjs/plugin/localeData";
+import 'dayjs/locale/zh-cn';
+dayjs.extend(weekday);
+dayjs.extend(localeData);
+dayjs.locale('zh-cn');
 
 const localeEnum = {
     zh_CN,
