4a740587925ae69c4020a0e333d3bdf40489b95e
wq1997 <2957009914@qq.com>
2023-10-16 09:32:30 +0800

    feat: 页面设计

--- src/components/DemandCard/index.jsx
+++ src/components/DemandCard/index.jsx
@@ -106,25 +106,7 @@ const DemandCard = ({
                                             {
                                                 key: '1',
                                                 label:(
-                                                    <a>发布</a>
-                                                )   
-                                            },
-                                            {
-                                                key: '2',
-                                                label:(
                                                     <a>审核</a>
-                                                )   
-                                            },
-                                            {
-                                                key: '3',
-                                                label:(
-                                                    <a>编辑</a>
-                                                )   
-                                            },
-                                            {
-                                                key: '4',
-                                                label:(
-                                                    <a>删除</a>
                                                 )   
                                             },
                                         ] }} 
