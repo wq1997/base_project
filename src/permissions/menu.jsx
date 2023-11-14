1f800995fac396bb2a51392bb26955f19db1bef1
wq1997 <2957009914@qq.com>
2023-11-14 18:02:33 +0800

    fix

--- src/permissions/menu.jsx
+++ src/permissions/menu.jsx
@@ -5,46 +5,94 @@ import { AppstoreOutlined, ToolOutlined, AlertOutlined, LineChartOutlined, Accou
 const { SubMenu } = Menu;
 
 const MenuList = [
-    ...menu
+    {
+        label: '总览',
+        key: '/index/home',
+        icon: <AppstoreOutlined />
+    },
+    {
+        label: '设备',
+        key: '/index/device',
+        icon: <ToolOutlined />,
+        children: [
+            { label: '储能系统', key: '/index/device/energy', type: 'big' },
+            { label: '光伏', key: '/index/device/photovoltaic', type: 'guang'},
+            { label: '充电桩', key: '/index/device/chargingStation', type: 'chong' },
+        ]
+    },
+    {
+        label: '报表',
+        key: '/index/statement',
+        icon: <LineChartOutlined />,
+        children: [
+            { label: '  统计数据', key: '/index/statement/statisticalData', },
+            { label: '数据对比', key: '/index/statement/dataComparison', },
+            { label: '报表导出', key: '/index/statement/reportExport', },
+        ]
+    },
+    {
+        label: '收益',
+        key: '/index/profit',
+        icon: <AccountBookOutlined />,
+        children: [
+            { label: '收益明细', key: '/index/profit/revenueDetails', },
+            { label: '收益统计', key: '/index/profit/revenueStatistics', },
+        ]
+    },
+    {
+        label: '告警',
+        key: '/index/alarm',
+        icon: <AlertOutlined/>,
+        children: [
+            { label: '实时告警', key: '/index/alarm/realtimeAlarm', },
+            { label: '历史告警', key: '/index/alarm/historyAlarm', },
+            { label: '告警规则', key: '/index/alarm/alarmRules', },
+        ]
+    },
+    {
+        label: '系统管理',
+        key: '/index/systemManagement',
+        icon: <SettingOutlined/>,
+        children: [
+            { label: '策略配置', key: '/index/systemManagement/policyConfiguration', },
+            { label: '用户管理', key: '/index/systemManagement/user', },
+            { label: '操作记录', key: '/index/systemManagement/operationRecords', },
+        ]
+    },
 ]
-const icon = [
-    <AppstoreOutlined />, <ToolOutlined />, <LineChartOutlined />, <AccountBookOutlined />,<AlertOutlined />,  <SettingOutlined />
-]
+const currentDivice = "chong";
 
-const getMenu = (menuList) => {
-    return menuList.map((menu, index) => {
-        if (menu.routes) {
+const getMenu = menuList => {
+    return menuList.map(menu => {
+        if (menu.children) {
             return (
-                <SubMenu key={menu.path} title={
-                    <span>
-                        {icon[index]}
-                        <span>{menu.name}</span>
-                    </span>
-                } >
-                    {getMenu(menu.routes)}
+                <SubMenu
+                    key={menu.key}
+                    title={menu.label}
+                    icon={menu.icon}
+                >
+                    {getMenu(menu.children)}
                 </SubMenu>
-            )
+            );
         } else {
-            if (menu.name === '总览') {
-                return (
-                    <Menu.Item key={menu.path}>
-                        <Link to={menu.path}>
-                            <span> {icon[index]}</span>
-                            <span style={{marginLeft:10}}>{menu.name}</span>
-                        </Link>
-                    </Menu.Item>
-                )
-            } else {
-                return (
-                    <Menu.Item key={menu.path}>
-                        <Link to={menu.path}>{menu.name}</Link>
-                    </Menu.Item>)
+            if(menu.type){
+                if(menu.type===currentDivice){
+                    return (
+                        <Menu.Item key={menu.key}>
+                            <Link to={menu.key}>{menu.label}</Link>
+                        </Menu.Item>
+                    );
+                }
+                return 
             }
-
-
+            return (
+                <Menu.Item key={menu.key} icon={menu.icon}>
+                    <Link to={menu.key}>{menu.label}</Link>
+                </Menu.Item>
+            );
         }
     });
-}
+};
 
 const MyMenu = () => {
     const location = useLocation();
