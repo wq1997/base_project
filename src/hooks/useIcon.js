c28403930a8d153d2660e44176a501f444d1a764
wq1997 <2957009914@qq.com>
2024-01-24 11:01:06 +0800

    替换右上角Icon

--- src/hooks/useIcon.js
+++ src/hooks/useIcon.js
@@ -3,10 +3,6 @@ import { createFromIconfontCN } from '@ant-design/icons';
 const useIcon = () => {
     return createFromIconfontCN({
         scriptUrl: [
-           '//at.alicdn.com/t/c/font_2123098_ncds5jynlwo.js',
-            '//at.alicdn.com/t/c/font_4191866_d1mfg6yoa7.js',
-          '//at.alicdn.com/t/c/font_2123098_68ehjg1myc.js',
-          '//at.alicdn.com/t/c/font_2123098_7jb9vzlsvmc.js',
           '//at.alicdn.com/t/c/font_2123098_h5icqgxdv68.js'
         ],
     })
