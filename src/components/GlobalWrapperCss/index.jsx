1243d3ecf053fcacd4df6532cd5ee223695d04e5
wq1997 <2957009914@qq.com>
2024-03-12 13:19:25 +0800

    fix

--- src/components/GlobalWrapperCss/index.jsx
+++ src/components/GlobalWrapperCss/index.jsx
@@ -1,9 +1,13 @@
 import { useEmotionCss } from '@ant-design/use-emotion-css';
+import { theme } from "antd";
 
 const GlobalWrapperCss = (props) => {
+    const { token } = theme.useToken();
     const globalStyle = useEmotionCss(()=>{
         return {
-
+            '.ant-table-wrapper .ant-table-thead >tr>th':{
+                 backgroundColor: token.tableHeaderColor
+            }   
         }
     })
     return (
