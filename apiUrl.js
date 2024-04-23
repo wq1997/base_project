3312fc5cd2d11ca1e6e12de548d9b2365a818cfa
wq1997 <2957009914@qq.com>
2024-04-23 16:02:28 +0800

    fix

--- apiUrl.js
+++ apiUrl.js
@@ -1,7 +1,4 @@
-const domain = ".sermatec-cloud.com";
-const serve = 1; // 1 国内 0 国外
-
 export default apiUrl = {
-    test: 'http://47.111.104.162:50106',
+    test: 'http://47.111.104.162:7884',
     prod: `http://47.111.104.162:50106`
 }
