229936b781ef07d09a4c4b5bbadfa99ab7a39b5f
wq1997 <2957009914@qq.com>
2024-04-26 13:19:20 +0800

    添加InputSelect组件

--- src/components/InputSelect/index.jsx
+++ src/components/InputSelect/index.jsx
@@ -1,7 +1,10 @@
-import { Select } from "antd";
-import { useState, useRef, useEffect } from "react";
+import { Select, theme } from "antd";
+import { useState, useRef } from "react";
+import { useEmotionCss } from '@ant-design/use-emotion-css';
+import { addColorAlpha } from "@/utils/utils";
 
 const InputSelect = (props) => {
+    const { token } = theme.useToken();
     const inputRef = useRef(null);
     const { value, options, onChange } = props;
     const [ myValue, setMyValue ] = useState(value||[]);
@@ -13,23 +16,58 @@ const InputSelect = (props) => {
         onChange(value);
     }
 
+    const onMyBlur = () => {
+        if(inputRef?.current) inputRef?.current?.blur();
+    }
+
+    const optionsItemStyle = useEmotionCss(()=>{
+        return {
+            padding: '5px 0 5px 5px',
+            cursor: 'pointer',
+            marginBottom: '5px',
+            "&:hover": {
+                background: addColorAlpha('#000000', 0.05)
+            }
+        }
+    })
+
     return (
         <Select 
             ref={inputRef}
             {...props} 
             value={myValue}
-            mode="multiple" 
+            showSearch
             options={myOptions}
             onChange={onMyChange} 
             filterOption={input => {
                 setInputContent(input);
             }}
-            onKeyDown={(e)=>{
-                if(e.keyCode===13){
-                    if(inputRef?.current) inputRef?.current?.blur();
-                    setMyOptions([...myOptions, {label: inputContent, value: inputContent}]);
-                    setMyValue([...myValue, inputContent]);
-                    onChange([...myValue, inputContent])
+            dropdownRender={_=>{
+                return options.map(item => {
+                    return (
+                        <div 
+                            key={item.value} 
+                            onClick={()=>{
+                                onMyChange(item.value);
+                                onMyBlur();
+                            }}
+                            className={optionsItemStyle}
+                            style={{
+                                background: myValue === item.value && addColorAlpha(token.colorPrimary, 0.05),
+                                height: item.visible===false?0:"auto"
+                            }}
+                        >
+                                {item.label}
+                        </div>
+                    )
+                });
+            }}
+            onBlur={()=>{
+                if(inputContent){
+                    onMyBlur();
+                    setMyOptions([...myOptions, {label: inputContent, value: inputContent, visible: false}]);
+                    setMyValue(inputContent);
+                    onChange(inputContent)
                     setInputContent('');
                 }
             }}
--- src/pages/test/index.jsx
+++ src/pages/test/index.jsx
@@ -15,69 +15,46 @@ const Test = () => {
       //   color: token.colorText
       // }}
     >
-      <InputSelect 
-        options={[
-          {label: '王清', value: '王清'}
-        ]}
-        onChange={(value) => {
-          console.log(value);
-        }}
-        style={{width: 200}}
-      />
-      <ToggleButton
-        freezeTime={5}
-        onClick={()=>{
-          alert("点击")
-        }}
-      >
-        下发
-      </ToggleButton>
-      <div style={{height: 200}}>
-        <div style={{width: '5px', height: '200px',position: "absolute", left: 200}}>
-          <div style={{width: 200, height: 5, position: 'absolute', left: 0, top: 0,transform: "rotate(90deg)", transformOrigin: '0px 0px', background: 'blue'}}>
-            <Flow img={flowImg}/>
-          </div>
-        </div>
-        <div style={{width: '5px', height: '200px',position: "absolute", left: 400}}>
-          <div style={{width: 200, height: 5,position: 'absolute', top: 200, transform: "rotate(-90deg)", transformOrigin: '0px 0px'}}>
-            <Flow img={flowImg}/>
-          </div>
-        </div>
-        <div style={{width: '1000px', height: '5px'}}>
-          <Flow img={flowImg}/>
-        </div>
-      </div>
-
-      <div style={{height: 100}}>
-        <ScrollTable 
-            columns={[
-              {title: "A", key: "A"},
-              {title: "B", key: "B"},
-              {title: "C", key: "C"},
-              {title: "D", key: "D"}
-            ]}
-            dataSource={[
-              {
-                A: "A",
-                B: "B",
-                C: "C",
-                D: "D"
-              },
-              {
-                A: "A",
-                B: "B",
-                C: "C",
-                D: "D"
-              },
-              {
-                A: "A",
-                B: "B",
-                C: "C",
-                D: "D"
-              }
-            ]}
+      <p>
+        <InputSelect 
+          options={[
+            {label: '王清', value: '王清'},
+            {label: '张宇', value: '张宇'}
+          ]}
+          onChange={(value) => {
+            console.log(value);
+          }}
+          style={{width: 200}}
         />
-      </div>
+      </p>
+      <p>
+        <ToggleButton
+          freezeTime={5}
+          onClick={()=>{
+            alert("点击")
+          }}
+        >
+          下发
+        </ToggleButton>
+      </p>
+      <p>
+        <div style={{height: 200}}>
+          <div style={{width: '5px', height: '200px',position: "absolute", left: 200}}>
+            <div style={{width: 200, height: 5, position: 'absolute', left: 0, top: 0,transform: "rotate(90deg)", transformOrigin: '0px 0px', background: 'blue'}}>
+              <Flow img={flowImg}/>
+            </div>
+          </div>
+          <div style={{width: '5px', height: '200px',position: "absolute", left: 400}}>
+            <div style={{width: 200, height: 5,position: 'absolute', top: 200, transform: "rotate(-90deg)", transformOrigin: '0px 0px'}}>
+              <Flow img={flowImg}/>
+            </div>
+          </div>
+          <div style={{width: '1000px', height: '5px'}}>
+            <Flow img={flowImg}/>
+          </div>
+        </div>
+      </p>
+
       <div style={{height: 100}}>
         <ScrollTable 
             columns={[
@@ -111,99 +88,101 @@ const Test = () => {
       <div 
         className={styles.element} 
       />
-      <Button
-        onClick={async ()=>{
-          const values = await form.validateFields();
-          console.log("values", values);
-        }}
-      >
-          保存
-      </Button>
-      <Form form={form}>
-        <Form.Item name="tableDatasource">
-          <EditTable.EditRowTable
-            showClear
-            showEdit
-            data={[
-              {
-                monPowers: 0,
-                tuePowers: '00:00~03:00',
-                wedPowers: '王清',
-                thuPowers: 0,
-                friPowers: 0,
-                satPowers: 0,
-                sunPowers: 0,
-              },
-              {
-                monPowers: 0,
-                tuePowers: '03:01~05:00',
-                wedPowers: '张宇',
-                thuPowers: "2024-01-02",
-                friPowers: 0,
-                satPowers: 0,
-                sunPowers: 0,
-              }
-            ]}
-            columns={[
-              {
-                title: 'Mon',
-                dataIndex: 'monPowers',
-                editable: true,
-                width: '12%',
-                inputType: 'InputNumber'
-              },
-              {
-                title: 'Tue',
-                dataIndex: 'tuePowers',
-                editable: true,
-                inputType: 'CustomDatePicker'
-              },
-              {
-                title: 'Wed',
-                dataIndex: 'wedPowers',
-                editable: true,
-                width: '12%',
-                inputType: 'Select',
-                options: [
-                  {
-                    value: '王清',
-                    name: '王清'
-                  },
-                  {
-                    value: '张宇',
-                    name: '张宇'
-                  }
-                ]
-              },
-              {
-                title: 'Thu',
-                dataIndex: 'thuPowers',
-                editable: true,
-                width: '12%',
-                inputType: 'DatePicker'
-              },
-              {
-                title: 'Fri',
-                dataIndex: 'friPowers',
-                editable: true,
-                width: '12%',
-              },
-              {
-                title: 'Sat',
-                dataIndex: 'satPowers',
-                editable: true,
-                width: '12%'
-              },
-              {
-                title: 'Sun',
-                dataIndex: 'sunPowers',
-                editable: true,
-                width: '12%'
-              },
-            ]}
-          />
-        </Form.Item>
-      </Form>
+      <p>
+        <Button
+          onClick={async ()=>{
+            const values = await form.validateFields();
+            console.log("values", values);
+          }}
+        >
+            保存
+        </Button>
+        <Form form={form}>
+          <Form.Item name="tableDatasource">
+            <EditTable.EditRowTable
+              showClear
+              showEdit
+              data={[
+                {
+                  monPowers: 0,
+                  tuePowers: '00:00~03:00',
+                  wedPowers: '王清',
+                  thuPowers: 0,
+                  friPowers: 0,
+                  satPowers: 0,
+                  sunPowers: 0,
+                },
+                {
+                  monPowers: 0,
+                  tuePowers: '03:01~05:00',
+                  wedPowers: '张宇',
+                  thuPowers: "2024-01-02",
+                  friPowers: 0,
+                  satPowers: 0,
+                  sunPowers: 0,
+                }
+              ]}
+              columns={[
+                {
+                  title: 'Mon',
+                  dataIndex: 'monPowers',
+                  editable: true,
+                  width: '12%',
+                  inputType: 'InputNumber'
+                },
+                {
+                  title: 'Tue',
+                  dataIndex: 'tuePowers',
+                  editable: true,
+                  inputType: 'CustomDatePicker'
+                },
+                {
+                  title: 'Wed',
+                  dataIndex: 'wedPowers',
+                  editable: true,
+                  width: '12%',
+                  inputType: 'Select',
+                  options: [
+                    {
+                      value: '王清',
+                      name: '王清'
+                    },
+                    {
+                      value: '张宇',
+                      name: '张宇'
+                    }
+                  ]
+                },
+                {
+                  title: 'Thu',
+                  dataIndex: 'thuPowers',
+                  editable: true,
+                  width: '12%',
+                  inputType: 'DatePicker'
+                },
+                {
+                  title: 'Fri',
+                  dataIndex: 'friPowers',
+                  editable: true,
+                  width: '12%',
+                },
+                {
+                  title: 'Sat',
+                  dataIndex: 'satPowers',
+                  editable: true,
+                  width: '12%'
+                },
+                {
+                  title: 'Sun',
+                  dataIndex: 'sunPowers',
+                  editable: true,
+                  width: '12%'
+                },
+              ]}
+            />
+          </Form.Item>
+        </Form>
+      </p>
       <ThemeBtn />
       <LocaleBtn />
       <p>{intl.formatMessage({id: 'handsome'})}</p>
