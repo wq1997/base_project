41ebe0e3c60cab33e33b4a7f13b00139f646594d
wq1997 <2957009914@qq.com>
2024-05-07 11:00:52 +0800

    fix

--- src/models/global.js
+++ src/models/global.js
@@ -4,7 +4,7 @@ export default {
     namespace: 'global',
 
     state: {
-      theme: getLocalStorage('theme') || "default",
+      theme: getLocalStorage('theme') || "dark",
       locale: getLocalStorage('locale')||"en-US"
     },
    
