fcb2addee928c023ea89f462c718c630ae1b281c
ZhangCoder0812 <zy15655867638@163.com>
2024-05-24 13:26:02 +0800

    -

--- apiUrl.js
+++ apiUrl.js
@@ -1,6 +1,6 @@
 export default apiUrl = {
-    // test1: "http://localhost:9000",
-     test1: "http://192.168.1.32:9000",
+     test1: "http://localhost:9000",
+    // test1: "http://192.168.1.32:9000",
     // test1: "http://192.168.1.32:8080/auth",
     // test2: "http://192.168.1.32:8080/main",
     // test3: "http://192.168.1.32:8080/energy",
@@ -8,3 +8,4 @@ export default apiUrl = {
     // prod2: "http://47.110.124.86:8080/main",
     // prod3: "http://47.110.124.86:8080/energy",
 };
+ 
