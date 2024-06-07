70fea23d11fed7612c756267cb51c4d6e7a67012
wq1997 <2957009914@qq.com>
2024-06-07 09:53:49 +0800

    fix

--- apiUrl.js
+++ apiUrl.js
@@ -1,6 +1,6 @@
 
 export default apiUrl = {
-    "test": 'http://192.168.1.42/api',
+    "test": 'http://192.168.1.82/api',
     "prod-in": 'http://192.168.1.42/api',
     socketURL:'http://192.168.1.42:9999'
 }
