14a9273802baae8f4adc80d9bbac5bb39aaef733
wq1997 <2957009914@qq.com>
2024-07-02 14:20:06 +0800

    fix

--- apiUrl.js
+++ apiUrl.js
@@ -1,7 +1,7 @@
 
 export default apiUrl = {
     "test": 'http://192.168.1.42/api',
-    "preProd": 'http://47.111.104.162:40108',
+    "preProd": 'http://47.111.104.162:40108/api',
     "prod": 'https://api.sermatec-cloud.com',
     socketURL:'http://192.168.1.42:9999'
 }
