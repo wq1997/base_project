2b8adcaa0afc0b5b54e8180bd1cf804d6a453ae3
wq1997 <2957009914@qq.com>
2024-07-05 14:12:48 +0800

    fix

--- src/pages/device/components/deviceRunDesc/index.jsx
+++ src/pages/device/components/deviceRunDesc/index.jsx
@@ -1,8 +1,6 @@
 
 import styles from "./index.less";
-import ball1Img from "../../../../../public/images/ball.gif";
-import ball2Img from "../../../../../public/images/ball.gif";
-import ball3Img from "../../../../../public/images/ball.gif";
+import ballImg from "../../../../assets/imges/ball.gif";
 import { useIntl } from "umi";
 
 const DeviceRunDesc = ({dataSource}) => {
@@ -13,21 +11,21 @@ const DeviceRunDesc = ({dataSource}) => {
             <div className={styles.ball1}>
                 <div className={styles.data1} style={{color: '#01FF23'}}>{dataSource?.onlineCount||0}</div>
                 <div className={styles.font}>{intl.formatMessage({id: '在线'})}</div>
-                <img src={ball1Img} />
+                <img src={ballImg} />
             </div>
 
             {/* 离线 */}
             <div className={styles.ball3}>
                 <div className={styles.data3} style={{color: '#E3E3E3'}}>{dataSource?.offlineCount||0}</div>
                 <div className={styles.font}>{intl.formatMessage({id: '离线'})}</div>
-                <img src={ball3Img} />
+                <img src={ballImg} />
             </div>
 
             {/* 告警 */}
             <div className={styles.ball2}>
                 <div className={styles.data2} style={{color: '#FF0000'}}>{dataSource?.nowAlarmCount||0}</div>
                 <div className={styles.font}>{intl.formatMessage({id: '告警'})}</div>
-                <img src={ball2Img} />
+                <img src={ballImg} />
             </div>
         </div>
     )
