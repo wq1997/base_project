5a42d18f3bedafd6097838ac36df69de5a4ff20a
ZhangCoder0812 <zy15655867638@163.com>
2024-07-08 16:16:39 +0800

    -

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,41 +1,46 @@
 import { ConfigProvider, theme as antdTheme } from "antd";
-import { Outlet, useSelector } from "umi";
+import { Outlet, history, useSelector, useLocation } from "umi";
 import { ThemeEnum, GlobalWrapperCss } from "@/components";
-import en_US from 'antd/locale/en_US';
-import zh_CN from 'antd/locale/zh_CN';
+import en_US from "antd/locale/en_US";
+import zh_CN from "antd/locale/zh_CN";
 import dayjs from "dayjs";
 import weekday from "dayjs/plugin/weekday";
 import localeData from "dayjs/plugin/localeData";
-import 'dayjs/locale/zh-cn';
+import "dayjs/locale/zh-cn";
 dayjs.extend(weekday);
 dayjs.extend(localeData);
-dayjs.locale('zh-cn');
+dayjs.locale("zh-cn");
 
 const localeEnum = {
     zh_CN,
-    en_US
-}
+    en_US,
+};
 
 const App = () => {
     const { theme, locale } = useSelector(state => state.global);
+
+    const location = useLocation();
+    if (location?.pathname === "/") {
+        history.push("/login");
+    }
 
     return (
         <ConfigProvider
             locale={localeEnum[locale]}
             theme={{
-                algorithm: theme === 'dark' ? antdTheme.darkAlgorithm : antdTheme.defaultAlgorithm,
+                algorithm: theme === "dark" ? antdTheme.darkAlgorithm : antdTheme.defaultAlgorithm,
                 token: {
                     ...ThemeEnum[theme],
-                    fontSize: 15
+                    fontSize: 15,
                 },
-                components: ThemeEnum[theme].components
+                components: ThemeEnum[theme].components,
             }}
         >
             <GlobalWrapperCss>
                 <Outlet />
             </GlobalWrapperCss>
         </ConfigProvider>
-    )
-}
+    );
+};
 
 export default App;
