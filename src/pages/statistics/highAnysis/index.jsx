357b67d1aea710ebb4d292c866cb85ed60820814
wq1997 <2957009914@qq.com>
2024-07-19 13:34:28 +0800

    fix

--- src/pages/statistics/highAnysis/index.jsx
+++ src/pages/statistics/highAnysis/index.jsx
@@ -19,43 +19,43 @@ import { downloadFile } from "@/utils/utils";
 
 const HighAnysis = () => {
     const intl = useIntl();
-    const {token} = theme.useToken();
+    const { token } = theme.useToken();
     const [form] = Form.useForm();
     const [dataSource, setDataSource] = useState([]);
     const [option, setOption] = useState({});
     const [plantDeviceList, setPlantDeviceList] = useState([]);
     const [packCellList, setPackCellList] = useState([]);
     const [loading, setLoading] = useState(false);
-    const [title, setTitle] = useState(`${intl.formatMessage({id: '高级分析'})}`);
+    const [title, setTitle] = useState(`${intl.formatMessage({ id: '高级分析' })}`);
 
     const dataProList = [
         {
             value: 'vol',
-            label: intl.formatMessage({id: '单体电压'}),
+            label: intl.formatMessage({ id: '单体电压' }),
             unit: 'V'
         },
         {
             value: 'temp',
-            label: intl.formatMessage({id: '单体温度'}),
+            label: intl.formatMessage({ id: '单体温度' }),
             unit: '℃'
         },
     ]
 
-    const getParams = async() => {
-        let format="YYYY-MM-DD";
+    const getParams = async () => {
+        let format = "YYYY-MM-DD";
         const values = await form.validateFields();
         let { date, currentPlantDevice, dataType, packCell } = values;
         date = date?.map(item => dayjs(item).format(format));
-        if(date?.length>3){
-            message.error(intl.formatMessage({id: '最多选择3个对比项'}));
+        if (date?.length > 3) {
+            message.error(intl.formatMessage({ id: '最多选择3个对比项' }));
             return;
         }
-        if(packCell?.length<2){
-            message.error(intl.formatMessage({id: '请选择电芯'}));
+        if (packCell?.length < 2) {
+            message.error(intl.formatMessage({ id: '请选择电芯' }));
             return;
         }
-        if(currentPlantDevice?.length<2){
-            message.error(intl.formatMessage({id: '请选择电站下具体设备'}));
+        if (currentPlantDevice?.length < 2) {
+            message.error(intl.formatMessage({ id: '请选择电站下具体设备' }));
             return;
         };
         let params = {
@@ -69,28 +69,28 @@ const HighAnysis = () => {
     }
 
     const initOption = async () => {
-        let format="YYYY-MM-DD";
+        let format = "YYYY-MM-DD";
         const values = await form.validateFields();
         let { dataType, date } = values;
-        const currentData = dataProList.find(data => data.value==dataType);
+        const currentData = dataProList.find(data => data.value == dataType);
         const name = `${currentData?.label}(${currentData?.unit})`;
-        if(currentData) setTitle(name);
+        if (currentData) setTitle(name);
 
-        let legendData = [], series = [], xData = [], min=0, max=100, splitNumber=5;
+        let legendData = [], series = [], xData = [], min = 0, max = 100, splitNumber = 5;
         date = date.map(item => dayjs(item).format(format));
         xData = dataSource?.[0]?.timeList;
 
-        if(dataType==='vol'){
-            const fieldList = [intl.formatMessage({id: '电压'})];
+        if (dataType === 'vol') {
+            const fieldList = [intl.formatMessage({ id: '电压' })];
 
             date?.forEach(item => {
-                fieldList.forEach(field=>{
+                fieldList.forEach(field => {
                     legendData.push(`${item} ${field}`);
                 })
             })
             legendData.forEach((legend, index) => {
                 const currentDate = legend?.split(' ')?.[0];
-                const currentData = dataSource?.find(item => item.date===currentDate);
+                const currentData = dataSource?.find(item => item.date === currentDate);
                 const filed = "vol";
                 const data = currentData?.energyData?.[filed];
                 series.push({
@@ -100,23 +100,23 @@ const HighAnysis = () => {
                     data: data?.map(item => item[1])
                 })
             })
-            min=2.5;
-            max=4;
-            splitNumber=10;
+            min = 2.5;
+            max = 4;
+            splitNumber = 10;
         }
 
-        if(dataType==='temp'){
-            const fieldList = [intl.formatMessage({id: '单体温度'}), intl.formatMessage({id: '左侧熔断器温度'}), intl.formatMessage({id: '右侧熔断器温度'}), intl.formatMessage({id: '负极极柱温度'}), intl.formatMessage({id: '正极极柱温度'})];
+        if (dataType === 'temp') {
+            const fieldList = [intl.formatMessage({ id: '单体温度' }), intl.formatMessage({ id: '左侧熔断器温度' }), intl.formatMessage({ id: '右侧熔断器温度' }), intl.formatMessage({ id: '负极极柱温度' }), intl.formatMessage({ id: '正极极柱温度' })];
 
             date?.forEach(item => {
-                fieldList.forEach(field=>{
+                fieldList.forEach(field => {
                     legendData.push(`${item} ${field}`);
                 })
             })
             legendData.forEach((legend, index) => {
                 const currentDate = legend?.split(' ')?.[0];
-                const currentData = dataSource?.find(item => item.date===currentDate);
-                const filed = index%4===0?"temp":(index%4===1?"tempLeft":(index%4===2?"tempRight":index%4===3?"tempNeg":"tempPos"));
+                const currentData = dataSource?.find(item => item.date === currentDate);
+                const filed = index % 4 === 0 ? "temp" : (index % 4 === 1 ? "tempLeft" : (index % 4 === 2 ? "tempRight" : index % 4 === 3 ? "tempNeg" : "tempPos"));
                 const data = currentData?.energyData?.[filed];
                 series.push({
                     name: legend,
@@ -126,9 +126,9 @@ const HighAnysis = () => {
                 })
             })
 
-            min=10;
-            max=40;
-            splitNumber=5;
+            min = 10;
+            max = 40;
+            splitNumber = 5;
         }
 
         const option = {
@@ -145,7 +145,7 @@ const HighAnysis = () => {
                 }
             },
             grid: {
-                top: '40',
+                top: dataType === 'temp' ? (date?.length === 1 ? 50 : 90) : 50,
                 right: '3%',
                 left: '5%',
                 bottom: '80'
@@ -176,10 +176,10 @@ const HighAnysis = () => {
                     show: false
                 },
                 splitLine: {
-                     show: true,
-                     lineStyle: {
-                         color: '#233e64'
-                     }
+                    show: true,
+                    lineStyle: {
+                        color: '#233e64'
+                    }
                 },
                 min,
                 max,
@@ -191,36 +191,36 @@ const HighAnysis = () => {
     }
 
     const getDtusOfPlant = async (plantList, plantId) => {
-        const res = await getDtusOfPlantServe({plantId});
-        if(res?.data?.data){
+        const res = await getDtusOfPlantServe({ plantId });
+        if (res?.data?.data) {
             let data = res?.data?.data;
-            if(data){
-                try{
+            if (data) {
+                try {
                     data = JSON.parse(data);
-                }catch{
+                } catch {
                     data = [];
                 }
-                data = data?.length>0?data?.map(item => {
+                data = data?.length > 0 ? data?.map(item => {
                     return {
                         value: item.id,
-                        label: item.name||intl.formatMessage({id: '设备无名称'})
+                        label: item.name || intl.formatMessage({ id: '设备无名称' })
                     }
-                }): [];
-                const currentIndex = plantList?.findIndex(item => item.value===plantId);
-                plantList[currentIndex].children=data;
+                }) : [];
+                const currentIndex = plantList?.findIndex(item => item.value === plantId);
+                plantList[currentIndex].children = data;
                 setPlantDeviceList([...plantList]);
 
                 const currentPlantDevice = await form.getFieldValue("currentPlantDevice")
-                if(currentPlantDevice?.length===0){
+                if (currentPlantDevice?.length === 0) {
                     let packCellList = [];
                     const packCellListRes = await getAnalyticsInitDataServe({ dtuId: data?.[0]?.value });
-                    if(packCellListRes?.data?.data?.packCellList){
+                    if (packCellListRes?.data?.data?.packCellList) {
                         const data = packCellListRes?.data?.data?.packCellList;
                         packCellList = data;
                     }
                     setPackCellList(packCellList);
                     form.setFieldsValue({
-                        currentPlantDevice:[plantId, data?.[0]?.value],
+                        currentPlantDevice: [plantId, data?.[0]?.value],
                         dataType: 'vol',
                         packCell: [packCellList?.[0]?.value, packCellList?.[0]?.children?.[0]?.value]
                     })
@@ -233,14 +233,14 @@ const HighAnysis = () => {
 
     const initPlantDevice = async () => {
         const res = await getFetchPlantListServe();
-        if(res?.data?.data){
+        if (res?.data?.data) {
             const data = res?.data?.data;
             const plantList = data?.plantList?.map((item, index) => {
                 return {
                     value: item.plantId,
                     label: item.name,
-                    disabled: data?.deviceCount?.[index]===0,
-                    children: data?.deviceCount?.[index]&&[
+                    disabled: data?.deviceCount?.[index] === 0,
+                    children: data?.deviceCount?.[index] && [
                         {
                             value: '',
                             label: ''
@@ -248,7 +248,7 @@ const HighAnysis = () => {
                     ]
                 }
             })
-            if(plantList?.length>0){
+            if (plantList?.length > 0) {
                 const findIndex = plantList.findIndex(item => !item.disabled);
                 getDtusOfPlant(plantList, plantList?.[findIndex]?.value)
             }
@@ -258,37 +258,37 @@ const HighAnysis = () => {
     const getDataSource = async (params) => {
         setLoading(true);
         const res = await monitorCurveServe(params);
-        if(res?.data?.data){
+        if (res?.data?.data) {
             setDataSource(res?.data?.data)
-        }else{
+        } else {
             setDataSource([]);
         }
         setLoading(false);
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         initOption();
     }, [dataSource]);
 
-    useEffect(()=>{
+    useEffect(() => {
         initPlantDevice();
     }, [])
 
     return (
-        <Space size={30} direction="vertical" style={{width: '100%', height:'100%', padding: 30}}>
+        <Space size={30} direction="vertical" style={{ width: '100%', height: '100%', padding: 30 }}>
             <Flex justify="center" align="center" gap={10}>
-                <Form 
-                    form={form} 
+                <Form
+                    form={form}
                     layout="inline"
                     initialValues={{
                         currentPlantDevice: [],
                         date: [dayjs(moment().format("YYYY-MM-DD"))]
                     }}
                     onValuesChange={async (value) => {
-                        if(value?.currentPlantDevice?.length===2){
+                        if (value?.currentPlantDevice?.length === 2) {
                             let packCellList = [];
                             const packCellListRes = await getAnalyticsInitDataServe({ dtuId: value?.currentPlantDevice?.[1] });
-                            if(packCellListRes?.data?.data?.packCellList?.length>0){
+                            if (packCellListRes?.data?.data?.packCellList?.length > 0) {
                                 const data = packCellListRes?.data?.data?.packCellList;
                                 packCellList = data;
                                 setPackCellList(packCellList);
@@ -296,10 +296,10 @@ const HighAnysis = () => {
                                     packCell: [packCellList?.[0]?.value, packCellList?.[0]?.children?.[0]?.value]
                                 })
                                 const params = await getParams();
-                                if(params){
+                                if (params) {
                                     getDataSource(params);
                                 }
-                            }else{
+                            } else {
                                 setPackCellList([]);
                                 form.setFieldsValue({
                                     packCell: []
@@ -309,87 +309,87 @@ const HighAnysis = () => {
                     }}
                 >
                     <Flex align="center">
-                        <Form.Item name={"currentPlantDevice"} label={intl.formatMessage({id: '设备'})}>
-                            <Cascader 
+                        <Form.Item name={"currentPlantDevice"} label={intl.formatMessage({ id: '设备' })}>
+                            <Cascader
                                 changeOnSelect
                                 options={plantDeviceList}
                                 onChange={async value => {
-                                    if(value?.length===1){
-                                        getDtusOfPlant(plantDeviceList,value[0])
+                                    if (value?.length === 1) {
+                                        getDtusOfPlant(plantDeviceList, value[0])
                                     }
                                 }}
-                                style={{width: '250px', height: 40}}
+                                style={{ width: '250px', height: 40 }}
                             />
                         </Form.Item>
-                        <Form.Item name={"dataType"} label={intl.formatMessage({id: '数据项'})}>
-                            <Select 
+                        <Form.Item name={"dataType"} label={intl.formatMessage({ id: '数据项' })}>
+                            <Select
                                 options={dataProList}
-                                style={{width: '250px', height: 40}}
+                                style={{ width: '250px', height: 40 }}
                             />
                         </Form.Item>
-                        <Form.Item name={"packCell"} label={intl.formatMessage({id: 'pack'})}>
-                            <Cascader 
+                        <Form.Item name={"packCell"} label={intl.formatMessage({ id: 'pack' })}>
+                            <Cascader
                                 changeOnSelect
                                 options={packCellList}
-                                style={{width: '250px', height: 40}}
+                                style={{ width: '250px', height: 40 }}
                             />
                         </Form.Item>
-                        <Tooltip title={intl.formatMessage({id: '最多选择3个对比项'})}>
-                            <Form.Item 
+                        <Tooltip title={intl.formatMessage({ id: '最多选择3个对比项' })}>
+                            <Form.Item
                                 name="date"
-                                label={intl.formatMessage({id: '日期'})}
+                                label={intl.formatMessage({ id: '日期' })}
                             >
-                                <DatePicker 
+                                <DatePicker
                                     multiple
                                     maxTagCount={1}
-                                    maxDate={dayjs(moment().format('YYYY-MM-DD'), 'YYYY-MM-DD')} 
-                                    style={{width: '300px', height: 40}}
+                                    maxDate={dayjs(moment().format('YYYY-MM-DD'), 'YYYY-MM-DD')}
+                                    style={{ width: '300px', height: 40 }}
                                     allowClear={false}
                                 />
                             </Form.Item>
                         </Tooltip>
                     </Flex>
                 </Form>
-                <Button 
-                    onClick={async ()=>{
+                <Button
+                    onClick={async () => {
                         const params = await getParams();
-                        if(params){
+                        if (params) {
                             getDataSource(params);
                         }
                     }}
                     type="primary"
-                    style={{padding: '0 20px', height: 40}}
+                    style={{ padding: '0 20px', height: 40 }}
                 >
-                    {intl.formatMessage({id: '查询'})}
+                    {intl.formatMessage({ id: '查询' })}
                 </Button>
-                <Button 
+                <Button
                     type="primary"
-                    onClick={async ()=>{
+                    onClick={async () => {
                         const params = await getParams();
                         const res = await exportAnalyticsServe(params);
-                        if(res?.data){
+                        if (res?.data) {
                             downloadFile({
-                                fileName: `${intl.formatMessage({id: '高级分析'})}.xlsx`,
+                                fileName: `${intl.formatMessage({ id: '高级分析' })}.xlsx`,
                                 content: res?.data
                             })
                         }
-                    }} 
-                    style={{ backgroundColor: token.defaultBg, padding: '0 20px', height: 40 }} 
+                    }}
+                    style={{ backgroundColor: token.defaultBg, padding: '0 20px', height: 40 }}
                 >
-                    {intl.formatMessage({id:'导出'})} Excel
+                    {intl.formatMessage({ id: '导出' })} Excel
                 </Button>
             </Flex>
             <Spin spinning={loading}>
-                <Space direction="vertical" style={{width: '100%'}}>
-                    <Title title={title}/>
-                    <div style={{width: '100%', height: 'calc(100vh - 250px)'}}>
+                <Space direction="vertical" style={{ width: '100%' }}>
+                    <Title title={title} />
+                    <div style={{ width: '100%', height: 'calc(100vh - 250px)' }}>
                         {
-                            dataSource?.length>0?
-                            <ReactECharts option={option} notMerge style={{width: '100%', height: '100%'}}/>
-                            :
-                            <div style={{position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)'}}>
-                                <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description={intl.formatMessage({id: '暂无数据'})} />
-                            </div>
+                            dataSource?.length > 0 ?
+                                <ReactECharts option={option} notMerge style={{ width: '100%', height: '100%' }} />
+                                :
+                                <div style={{ position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)' }}>
+                                    <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description={intl.formatMessage({ id: '暂无数据' })} />
+                                </div>
                         }
                     </div>
                 </Space>
