12b5150e6a436bc9ee3eb3e2d124bd919987a3b5
wq1997 <2957009914@qq.com>
2024-07-22 11:28:38 +0800

    fix

--- apiUrl.js
+++ apiUrl.js
@@ -1,7 +1,7 @@
 export default apiUrl = {
-    test1: 'http://192.168.1.82:8080/auth',
-    test2: 'http://192.168.1.82:8080/main',
-    test3: 'http://192.168.1.82:8080/energy',
+    test1: 'http://192.168.1.82:8091/web-api',
+    test2: 'http://192.168.1.82:8091/web-api',
+    test3: 'http://192.168.1.82:8091/web-api',
     prod1: 'https://jsvpp.sermatec-cloud.com/api/auth',
     prod2: 'https://jsvpp.sermatec-cloud.com/api/main',
     prod3: 'https://jsvpp.sermatec-cloud.com/api/energy'
