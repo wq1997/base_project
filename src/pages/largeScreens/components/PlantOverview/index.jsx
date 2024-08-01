7d238cc0e56f2059f35beb2ab05a7b8eaee6ac62
wq1997 <2957009914@qq.com>
2024-08-01 17:37:56 +0800

    fix

--- src/pages/largeScreens/components/PlantOverview/index.jsx
+++ src/pages/largeScreens/components/PlantOverview/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import classNames from "classnames";
-import { Form, Select, Input, Badge } from "antd";
+import { Form, Select, Tooltip, Badge } from "antd";
 import styles from "./index.less";
 import Card from "../Card";
 import { Charts3D, Charts2_5D, ScrollTable } from "@/components";
@@ -21,7 +21,7 @@ const Index = ({ data: { total, totalCapacity, totalPlant } }) => {
                                 "#01F29B",
                                 "#FFF073", // 黄色
                             ]}
-                            data={total?.sort((a,b)=>b.value-a.value)}
+                            data={total?.sort((a, b) => b.value - a.value)}
                         />
                     </div>
                     <div className={styles.areaContentData}>
@@ -31,9 +31,11 @@ const Index = ({ data: { total, totalCapacity, totalPlant } }) => {
                                 text={<span style={{ color: "#FFFFFF" }}>总电站个数</span>}
                             />
                             <div className={styles.areaContentDataItemData}>
-                                <span className={styles.areaContentDataItemData1}>
-                                    {totalPlant}
-                                </span>
+                                <Tooltip title={totalPlant}>
+                                    <span className={styles.areaContentDataItemData1}>
+                                        {totalPlant}
+                                    </span>
+                                </Tooltip>
                                 <span className={styles.areaContentDataItemData2}>个</span>
                             </div>
                         </div>
@@ -43,15 +45,19 @@ const Index = ({ data: { total, totalCapacity, totalPlant } }) => {
                                 text={<span style={{ color: "#FFFFFF" }}>总装机容量</span>}
                             />
                             <div className={styles.areaContentDataItemData}>
-                                <span className={styles.areaContentDataItemData1}>
-                                    {totalCapacity?._1}
-                                </span>
+                                <Tooltip title={totalCapacity?._1}>
+                                    <span className={styles.areaContentDataItemData1}>
+                                        {totalCapacity?._1}
+                                    </span>
+                                </Tooltip>
                                 <span className={styles.areaContentDataItemData2}>MW</span>
-                                /
-                                <span className={styles.areaContentDataItemData1}>
-                                    {totalCapacity?._2}
-                                </span>
-                                <span className={styles.areaContentDataItemData2}>MWH</span>
+                                <span style={{ fontSize: 18, display: 'inline-block', margin: '0 5px'}}>/</span>
+                                <Tooltip title={totalCapacity?._2}>
+                                    <span className={styles.areaContentDataItemData1}>
+                                        {totalCapacity?._2}
+                                    </span>
+                                </Tooltip>
+                                <span className={styles.areaContentDataItemData2}>MWh</span>
                             </div>
                         </div>
                     </div>
