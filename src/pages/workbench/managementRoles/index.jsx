215588f02baa32e78671d7a3bef595abfa40c2cb
wq1997 <2957009914@qq.com>
2024-08-05 10:44:26 +0800

    fix

--- src/pages/workbench/managementRoles/index.jsx
+++ src/pages/workbench/managementRoles/index.jsx
@@ -1,12 +1,13 @@
-import { Select, Space } from "antd";
+import { Select, Space, theme as antdTheme } from "antd";
 import Board from "./Board";
 import Total from "./Total";
 import PersonnelTasks from "./PersonnelTasks";
 import "./index.less";
 
 const ManagementRoles = () => {
+    const { token } = antdTheme.useToken();
     return (
-        <div className="management-roles">
+        <div className="management-roles" style={{background: token.color14}}>
             <Board />
             <Total />
             <PersonnelTasks />
