80cbaa07f2016476f1cd3d33f7a7ee69877133d9
ZhangCoder0812 <zy15655867638@163.com>
2024-08-12 16:44:45 +0800

    -

--- src/services/index.js
+++ src/services/index.js
@@ -1,28 +1,27 @@
 import axiosInstance from "./request";
 const { API_URL } = process.env;
-const URL = API_URL == "http://47.110.124.86:8765" ? `${API_URL}/bdt` : API_URL;
 
 export const login = payload => {
-    const url = `${URL}/user/login`;
+    const url = `${API_URL}/user/login`;
     return axiosInstance.post(url, payload);
 };
 
 export const register = payload => {
-    const url = `${URL}/cpu-registration/register`;
+    const url = `${API_URL}/cpu-registration/register`;
     return axiosInstance.post(url, payload);
 };
 
 export const getUserist = payload => {
-    const url = `${URL}/cpu-registration/find-page`;
+    const url = `${API_URL}/cpu-registration/find-page`;
     return axiosInstance.post(url, payload);
 };
 
 export const handleApprove = payload => {
-    const url = `${URL}/cpu-registration/approve`;
+    const url = `${API_URL}/cpu-registration/approve`;
     return axiosInstance.post(url, payload);
 };
 
 export const handleDelete = id => {
-    const url = `${URL}/cpu-registration/delete/${id}`;
+    const url = `${API_URL}/cpu-registration/delete/${id}`;
     return axiosInstance.delete(url);
 };
