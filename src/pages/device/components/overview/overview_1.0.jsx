62134ca3d9559656707176e506eefcb4cd6b20ac
wq1997 <2957009914@qq.com>
2024-08-20 17:52:28 +0800

    fix

--- src/pages/device/components/overview/overview_1.0.jsx
+++ src/pages/device/components/overview/overview_1.0.jsx
@@ -330,7 +330,7 @@ const OverView = ({ deviceVersion, sn }) => {
                                                     <div className={styles.rightData1}>{item.data}</div>
                                                     <div className={styles.rightData2} style={{ color: token.color25 }}>{intl.formatMessage({ id: item.title })}</div>
                                                 </div>
-                                                {/* <img src={theme === "dark" ? leftTopBgImg : leftTopWhiteBgImg} /> */}
+                                                <img src={theme === "dark" ? leftTopBgImg : leftTopWhiteBgImg} />
                                             </div>
                                         </div>
                                     )
