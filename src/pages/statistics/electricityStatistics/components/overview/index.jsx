39297626c09686b4972d79f00058e46301fb4704
aiyaoke <592048781@qq.com>
2024-08-27 20:16:32 +0800

    a

--- src/pages/statistics/electricityStatistics/components/overview/index.jsx
+++ src/pages/statistics/electricityStatistics/components/overview/index.jsx
@@ -219,10 +219,10 @@ function Com(props) {
         setCurrntGrid(e);
     };
     const getGrid = async () => {
-        let { data: grid } = await getGridPointList({
+        let res = await getGridPointList({
             plantId: localStorage.getItem('plantId')
         })
-        setGrids([{id:"ALL",gridPointName:getTranslation('电站总计')},...grid?.data]);
+        setGrids([{id:"ALL",gridPointName:getTranslation('电站总计')},...res?.data?.grid?.data]);
         setCurrntGrid('ALL');
         getData();
     }
