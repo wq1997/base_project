14ef07bf3de58ebb3bc964457438d74309f65016
aiyaoke <592048781@qq.com>
2024-09-24 15:10:34 +0800

    1

--- apiUrl.js
+++ apiUrl.js
@@ -1,8 +1,7 @@
-const domain = ".sermatec-cloud.com";
-const serve = 1; // 1 国内 0 国外
+
 
 export default apiUrl = {
-    // test: 'http://47.111.104.162:40009/api/',
-    test: 'http://192.168.1.42:9799',
-    prod: `https://${serve ? "api" : "api1"}${domain}`
+    test: 'http://47.111.104.162:40009/api/',
+    // test: 'http://192.168.1.42:9799',
+    prod: `https://abroad-energy.sermatec-cloud.com/api/`
 }   
