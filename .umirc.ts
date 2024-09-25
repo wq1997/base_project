a43e8cd22ec8a631c87a8369638bee5951ee3830
wq1997 <2957009914@qq.com>
2024-09-25 16:36:50 +0800

    fix

--- .umirc.ts
+++ .umirc.ts
@@ -8,110 +8,120 @@ const CompressionWebpackPlugin = require("compression-webpack-plugin");
 const { UMI_ENV = "" } = process.env;
 
 function OutputPathName(env: string) {
-    if (env) {
-        return `${env}-dist`;
-    }
-    return "dist";
+  if (env) {
+    return `${env}-dist`;
+  }
+  return "dist";
 }
 
 export default defineConfig({
-    routes,
-    npmClient: "pnpm",
-    plugins: ["@umijs/plugins/dist/dva", "@umijs/plugins/dist/locale"],
-    dva: {},
-    locale: {
-        default: "zh-CN",
-    },
-    outputPath: OutputPathName(UMI_ENV),
-    define: {
-        "process.env.API_URL": apiUrl[UMI_ENV || "test"],
-    },
-    alias: {
-        "@/permissions": path.resolve(__dirname, "src/permissions"),
-        "@/hooks": path.resolve(__dirname, "src/hooks"),
-        "@/utils": path.resolve(__dirname, "src/utils"),
-    },
-    headScripts: [
-        //  "https://webapi.amap.com/maps?v=1.4.15&key=2dca0cb2ced6ced6030c26376186faee&plugin=AMap.MarkerCluster",
-        "https://webapi.amap.com/maps?v=2.0&key=703e127456c7052706f27a37144c9c4d&plugin=AMap.MarkerCluster",
-    ],
-    chainWebpack: config => {
-        config
-            .plugin("replace")
-            .use(require("webpack").ContextReplacementPlugin)
-            .tap(() => {
-                return [/moment[/\\]locales$/, /zh-cn/];
-            });
-        config.merge({
-            optimization: {
-                minimize: true,
-                splitChunks: {
-                    chunks: "async",
-                    minSize: 30000,
-                    minChunks: 2,
-                    automaticNameDelimiter: ".",
-                    cacheGroups: {
-                        react: {
-                            name: "react",
-                            priority: 20,
-                            test: /[\\/]node_modules[\\/](react|react-dom|react-dom-router)[\\/]/,
-                        },
-                        echarts: {
-                            name: "echarts",
-                            chunks: "async",
-                            test: /[\\/]node_modules[\\/](echarts|zrender)[\\/]/,
-                            priority: 10,
-                            enforce: true,
-                        },
-                        antdesigns: {
-                            name: "antdesigns",
-                            chunks: "all",
-                            test: /[\\/]node_modules[\\/](antd|@ant-design|antd-mobile)/,
-                            priority: 10,
-                            enforce: true,
-                        },
-                        ahooks: {
-                            name: "ahooks",
-                            chunks: "all",
-                            test: /[\\/]node_modules[\\/](ahooks)/,
-                            priority: 10,
-                            enforce: true,
-                        },
-                        antv: {
-                            name: "antv",
-                            chunks: "all",
-                            test: /[\\/]node_modules[\\/](@antv)[\\/]/,
-                            priority: 10,
-                            enforce: true,
-                        },
-                        lodash: {
-                            name: "lodash",
-                            test: /[\\/]node_modules[\\/]lodash[\\/]/,
-                            priority: -2,
-                            enforce: true,
-                        },
-                        vendors: {
-                            name: "vendors",
-                            test({ resource }: any) {
-                                return /[\\/]node_modules[\\/]/.test(resource);
-                            },
-                            priority: -11,
-                            enforce: true,
-                        },
-                    },
-                },
+  routes,
+  npmClient: "pnpm",
+  plugins: ["@umijs/plugins/dist/dva", "@umijs/plugins/dist/locale"],
+  dva: {},
+  locale: {
+    default: "zh-CN",
+  },
+  outputPath: OutputPathName(UMI_ENV),
+  define: {
+    "process.env.API_URL": apiUrl[UMI_ENV || "test"],
+  },
+  alias: {
+    "@/permissions": path.resolve(__dirname, "src/permissions"),
+    "@/hooks": path.resolve(__dirname, "src/hooks"),
+    "@/utils": path.resolve(__dirname, "src/utils"),
+  },
+  headScripts: [
+    //  "https://webapi.amap.com/maps?v=1.4.15&key=2dca0cb2ced6ced6030c26376186faee&plugin=AMap.MarkerCluster",
+    "https://webapi.amap.com/maps?v=2.0&key=703e127456c7052706f27a37144c9c4d&plugin=AMap.MarkerCluster",
+  ],
+  codeSplitting: {
+    jsStrategy: 'granularChunks',
+  },
+  cssMinifier: 'esbuild',
+  cssMinifierOptions: {
+    minifyWhitespace: true,
+    minifySyntax: true,
+  },
+  hash: true,
+  mfsu: {},
+  chainWebpack: config => {
+    config
+      .plugin("replace")
+      .use(require("webpack").ContextReplacementPlugin)
+      .tap(() => {
+        return [/moment[/\\]locales$/, /zh-cn/];
+      });
+    config.merge({
+      optimization: {
+        minimize: true,
+        splitChunks: {
+          chunks: "async",
+          minSize: 30000,
+          minChunks: 2,
+          automaticNameDelimiter: ".",
+          cacheGroups: {
+            react: {
+              name: "react",
+              priority: 20,
+              test: /[\\/]node_modules[\\/](react|react-dom|react-dom-router)[\\/]/,
             },
-        });
-        if (process.env.NODE_ENV === "production") {
-            config.plugin("compression-webpack-plugin").use(
-                new CompressionWebpackPlugin({
-                    algorithm: "gzip",
-                    test: new RegExp("\\.(" + prodGzipList.join("|") + ")$"),
-                    threshold: 10240,
-                    minRatio: 0.6,
-                    deleteOriginalAssets: false,
-                })
-            );
-        }
-    },
+            echarts: {
+              name: "echarts",
+              chunks: "async",
+              test: /[\\/]node_modules[\\/](echarts|zrender)[\\/]/,
+              priority: 10,
+              enforce: true,
+            },
+            antdesigns: {
+              name: "antdesigns",
+              chunks: "all",
+              test: /[\\/]node_modules[\\/](antd|@ant-design|antd-mobile)/,
+              priority: 10,
+              enforce: true,
+            },
+            ahooks: {
+              name: "ahooks",
+              chunks: "all",
+              test: /[\\/]node_modules[\\/](ahooks)/,
+              priority: 10,
+              enforce: true,
+            },
+            antv: {
+              name: "antv",
+              chunks: "all",
+              test: /[\\/]node_modules[\\/](@antv)[\\/]/,
+              priority: 10,
+              enforce: true,
+            },
+            lodash: {
+              name: "lodash",
+              test: /[\\/]node_modules[\\/]lodash[\\/]/,
+              priority: -2,
+              enforce: true,
+            },
+            vendors: {
+              name: "vendors",
+              test({ resource }: any) {
+                return /[\\/]node_modules[\\/]/.test(resource);
+              },
+              priority: -11,
+              enforce: true,
+            },
+          },
+        },
+      },
+    });
+    if (process.env.NODE_ENV === "production") {
+      config.plugin("compression-webpack-plugin").use(
+        new CompressionWebpackPlugin({
+          algorithm: "gzip",
+          test: new RegExp("\\.(" + prodGzipList.join("|") + ")$"),
+          threshold: 10240,
+          minRatio: 0.6,
+          deleteOriginalAssets: false,
+        })
+      );
+    }
+  },
 });
