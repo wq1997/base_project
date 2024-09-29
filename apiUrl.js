ddc10c02e90aab449f07f283a2cb7823a653c0ba
aiyaoke <592048781@qq.com>
2024-09-29 13:03:40 +0800

    1

--- apiUrl.js
+++ apiUrl.js
@@ -1,7 +1,8 @@
 
 
 export default apiUrl = {
-    test: 'http://47.111.104.162:40009/api/',
+    // test: 'http://47.111.104.162:40009/api/',
     // test: 'http://192.168.1.42:9799',
+    test:'https://abroad-energy.sermatec-cloud.com/api/',
     prod: `https://abroad-energy.sermatec-cloud.com/api/`
 }     
