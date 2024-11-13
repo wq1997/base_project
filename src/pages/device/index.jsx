c97c10c55aec43b5abd7abcccf720efa3e852a79
PengYi <1041166936@qq.com>
2024-11-13 16:51:44 +0800

    #聚合平台首页去掉路由中的Token重定向

--- src/pages/device/index.jsx
+++ src/pages/device/index.jsx
@@ -22,6 +22,7 @@ import { useEmotionCss } from '@ant-design/use-emotion-css';
 import Table from "./components/table";
 import Map from './components/map';
 import dayjs from 'dayjs';
+import {getUrlParams, setLocalStorage} from "@/utils/utils";
 const { Option } = Select;
 
 const RealtimeAlarm = () => {
@@ -119,6 +120,11 @@ const RealtimeAlarm = () => {
     ])
 
     useEffect(() => {
+        const params = getUrlParams(window.location.search);
+        if(params?.token){
+            setLocalStorage('Token',params.token);
+            history.push('/index/device');
+        }
         getAllPlant();
     }, [locale])
 
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -117,3 +117,18 @@ export function download(url, filename) {
 
 export const cloneObject = (object) => JSON.parse(JSON.stringify(object));
 
+export const getUrlParams = url => {
+    if (!url) return {};
+    // 通过 ? 分割获取后面的参数字符串
+    let urlStr = url.split("?")[1];
+    // 创建空对象存储参数
+    let obj = {};
+    // 再通过 & 将每一个参数单独分割出来
+    let paramsArr = urlStr.split("&");
+    for (let i = 0, len = paramsArr.length; i < len; i++) {
+        // 再通过 = 将每一个参数分割为 key:value 的形式
+        let arr = paramsArr[i].split("=");
+        obj[arr[0]] = arr[1];
+    }
+    return obj;
+};
